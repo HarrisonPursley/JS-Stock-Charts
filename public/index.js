116778da63dfd88e699bbf88ddef3956efa3391e
Zeus <purslh1@unlv.nevada.edu>
2024-05-21 18:23:29 -0700

    0.0.1 Began work on main inside index.js, started chart work

--- public/index.js
+++ public/index.js
@@ -5,5 +5,26 @@ async function main() {
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
 }
+// KEY c21056ad0595494e8370f26f923720a9
+main(){
+    const response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=c21056ad0595494e8370f26f923720a9')
+    const result = await response.json()
+    const {GME, MSFT, DIS, BNTX} = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
 
-main()
+    stocks.forEach(stock => stock.values.reverse())
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    });
+    
+}
